88b78903b2748890091995431a41a664f6bd45dd
Mario Angst <mario.angst@gmail.com>
2024-12-08 23:45:45 +0100

    test: basic tests for dimensions

--- tests/testthat/test-core.R
+++ tests/testthat/test-core.R
@@ -1,3 +1,53 @@
-test_that("outputting png image works", {
-  expect_equal(2 * 2, 4)
+test_that("magick to ayab creates desired width and length transformation", {
+  expect_equal(
+    magick::rose |>
+    rayab::magick_to_ayab(width = 10) |>
+    magick::image_info() |>
+    dplyr::pull(width)
+    , 10)
+  expect_equal(
+    magick::rose |>
+      rayab::magick_to_ayab(height = 20) |>
+      magick::image_info() |>
+      dplyr::pull(height)
+    , 20)
+  expect_equal(
+    magick::rose |>
+      rayab::magick_to_ayab(width = 10, height = 20) |>
+      magick::image_info() |>
+      dplyr::pull(height)
+    , 20)
+  expect_equal(
+    magick::rose |>
+      rayab::magick_to_ayab(width = 10, height = 20) |>
+      magick::image_info() |>
+      dplyr::pull(width)
+    , 10)
+})
+
+test_that("ggplot to ayab creates desired width and length transformation", {
+  expect_equal(
+    make_test_plot() |>
+      rayab::ggplot_to_ayab(width = 10) |>
+      magick::image_info() |>
+      dplyr::pull(width)
+    , 10)
+  expect_equal(
+    make_test_plot() |>
+      rayab::ggplot_to_ayab(height = 20) |>
+      magick::image_info() |>
+      dplyr::pull(height)
+    , 20)
+  expect_equal(
+    make_test_plot() |>
+      rayab::ggplot_to_ayab(width = 10, height = 20) |>
+      magick::image_info() |>
+      dplyr::pull(height)
+    , 20)
+  expect_equal(
+    make_test_plot() |>
+      rayab::ggplot_to_ayab(width = 10, height = 20) |>
+      magick::image_info() |>
+      dplyr::pull(width)
+    , 10)
 })
