0fec1afa522f7afe184860d8cf4926ae9298c060
Mario Angst <mario.angst@gmail.com>
2024-12-08 12:49:10 +0100

    feat: added end-to-end functions, added height parameter to conversion

--- R/output.R
+++ R/output.R
@@ -23,7 +23,7 @@ make_test_plot_complex <- function(){
 
 make_test_plot_complex_lines <- function(){
   nile_flow <- data.frame(flow = as.matrix(Nile),
-                          year = time(Nile))
+                          year = stats::time(Nile))
   nile_flow$crocodiles <- sample(c("big","small","none"),
                                  size = nrow(nile_flow),
                                  replace = TRUE)
@@ -47,6 +47,7 @@ make_text_img <- function(input_text){
 
 magick_ayab <- function(fig,
                         width = NULL,
+                        height = NULL,
                         bw_method = NULL){
   BW_METHODS <- c("quantize","threshold")
   if(is.null(bw_method) || !(bw_method %in% BW_METHODS)){
@@ -60,21 +61,26 @@ magick_ayab <- function(fig,
   }
   info <- magick::image_info(fig)
   img_scale <- info$width/info$height
+  # set output height by default based on ratio of orginal image
+  if (is.null(height)){
+    height <- info$width*img_scale
+  }
+  logger::log_info(glue::glue("Using {height} as height conversion"))
   # scale and make black and white
   if (bw_method == "quantize"){
     fig_scaled_bw <-
-      magick::image_scale(fig,glue::glue("{width}x{info$width*img_scale}")) |>
+      magick::image_scale(fig,glue::glue("{width}x{height}")) |>
       magick::image_quantize(max = 2, colorspace = "Gray")
   }
   if (bw_method == "threshold"){
     fig_scaled_bw <-
-      magick::image_scale(fig,glue::glue("{width}x{info$width*img_scale}")) |>
+      magick::image_scale(fig,glue::glue("{width}x{height}")) |>
       magick::image_convert(type = "Bilevel") |>
       magick::image_threshold(type = "black", threshold = "50%") |>
       magick::image_threshold(type = "white", threshold = "50%")
   }
   # upscale again to make pixelated, if original image was larger
-  if(info$width > width){
+  if(info$width > width | info$height > height){
     fig_pixelated <- magick::image_scale(fig_scaled_bw,
                                          glue::glue("{info$width}x{info$height}"))
   }
@@ -82,7 +88,9 @@ magick_ayab <- function(fig,
   else{
     fig_pixelated <- fig_scaled_bw
   }
-  fig_pixelated
+  # and now, output with the given dimensions (force with !):
+  fig_pixelated |>
+    magick::image_scale(geometry = glue::glue("{width}x{height}!"))
 }
 
 ggplot_to_magick <- function(p){
@@ -90,6 +98,27 @@ ggplot_to_magick <- function(p){
   print(p)
   dev.off()
   return(fig)
+}
+
+ggplot_to_ayab <- function(p,
+                           width = NULL,
+                           height = NULL,
+                           bw_method = NULL){
+  p |>
+    ggplot_void() |>
+    ggplot_to_magick() |>
+    magick_ayab(width = width,
+                height = height,
+                bw_method = bw_method)
+}
+
+text_to_ayab <- function(input_text,
+                         width = NULL,
+                         height = NULL){
+  input_text |>
+    make_text_img() |>
+    magick_ayab(width = width,
+                height = height)
 }
 
 save_ayab_png <- function(fig, filename){
---
+++ R/rayab-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom datasets Nile
+#' @importFrom grDevices dev.off
+#' @importFrom stats rbinom
+## usethis namespace: end
+NULL
